eed34e87041739c50a8f36b88b7f5365c58ee724
Guelmis Cortina <cguelmis@yahoo.com>
2018-02-15 15:19:40 -0500

    get results back from the google api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const fetch = require('node-fetch');
+const config = require('./config.js');
 
 const server = express();
 
@@ -7,7 +9,26 @@ server.use(bodyParser.json());
 const PORT = config.port;
 const GMAPS_KEY = config.gmaps.apiKey;
 
+server.get('/place', (req, res) => {
+	const { place } = req.query;
+	// if (!place) {
+	// 	res.send({ error: "Input place" });
+	// }
+	console.log('place ->', place);
+	console.log('GMAPS_KEY', GMAPS_KEY);
+	let fetchBody = '';
+	let fetchText = '';
+	//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&types=food&key=YOUR_API_KEY
+	fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${GMAPS_KEY}`)
+		.then(res => {
+			res.json().then(json => {
+				console.log('results', json.results);
+			})
+		})
+		.catch(err => console.log('error:', err));
 
+	res.send(fetchText);
+});
 
 server.listen(PORT, err => {
 	if (err) {
